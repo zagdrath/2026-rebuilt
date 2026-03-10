c3e04a33be065e5610fa4af2e95c51c3bf7bb65f
Cody Wellman <cwellman@quartz.systems>
2026-03-10 11:36:13 -0400

    Refactor ClimberSubsystem for height control

--- src/main/java/frc/team3602/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/team3602/robot/subsystems/ClimberSubsystem.java
@@ -14,60 +14,50 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team3602.robot.Constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
+    /* Motors */
     private final TalonFX climberMotor = new TalonFX(ClimberConstants.kClimberMotorID);
 
-    private double pivotSetPoint = 0.0;
-    public double pidEffort;
-    public double ffEffort;
-    public double totalEffort;
+    /* Controls */
+    private final ArmFeedforward climberFeedforward = new ArmFeedforward(0, 0.69, 15.79, 0.07);
+    private final PIDController climberController = new PIDController(43.19, 0, 0.19);
+    
+    private double heightInches = 0.0;
 
     public ClimberSubsystem() {
-        var ClimbMotorConfig = new MotorOutputConfigs();
-        var ClimbLimitConfig = new CurrentLimitsConfigs();
+        configClimberSubsys();
     }
 
-    public double climberPosition() {
+    public double getHeight() {
        return (climberMotor.getRotorPosition().getValueAsDouble() * (Math.PI * 2.15) /  20.25);
     }
 
-    private final PIDController climbController = new PIDController(43.19, 0, 0.19);
-    private final ArmFeedforward climbFeedforward = new ArmFeedforward(0, 0.69, 15.79, 0.07);
-
-    public Command setClimbHeight(double setpoint) {
-        return runOnce(() ->  pivotSetPoint = setpoint);
+    public Command setHeight(DoubleSupplier heightInches) {
+        return runOnce(() ->  this.heightInches = heightInches.getAsDouble());
     }
 
-   //Climber
-    public Command raiseClimber(){
-        return this.setClimbHeight(20);
+    public double getEffort() {
+        var ffEffort climberFeedforward.calculate(Units.degreestoRadians(climberHeight), 0.0);
+        var pidEffort climberController.calculate(getHeight(), climberHeight);
+
+        return ffEffort + pidEffort;
     }
 
-    public Command lowerClimber(){
-        return this.setClimbHeight(5);
-    }
-
-    public Command setPosition() {
+    public Command holdHeight() {
         return run(() -> {
-            var pidEffort = climbController.calculate(climberPosition(), pivotSetPoint);
-            this.pidEffort = pidEffort;
-
-            var ffEffort = climbFeedforward.calculate(0, 0);
-            this.ffEffort = ffEffort;
-
-            var totalEffort = pidEffort + ffEffort;
-            this.totalEffort = totalEffort;
-
-            climberMotor.setVoltage(totalEffort);
+            climberMotor.setVoltage(getEffort());
         });
     }
 
+    public Command stopClimber() {
+        return runOnce(() -> climberMotor.setVoltage(0.0));
+    }
+    
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("PID Effort", ffEffort);
-        SmartDashboard.putNumber("Feedforward Effort", ffEffort);
-        SmartDashboard.putNumber("Total Effort", totalEffort);
+        
+    }
 
-        SmartDashboard.putNumber("Climber Setpoint", pivotSetPoint);
-        SmartDashboard.putNumber("Climber Measurement", climberPosition());
+    private void configClimberSubsys() {
+        
     }
 }
